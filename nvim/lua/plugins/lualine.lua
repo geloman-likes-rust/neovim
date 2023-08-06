d1bb33c7a301509598d6277e3c2c2bb5fa0d553f
geloman <geloman-likes-rust@users.noreply.github.com>
2023-08-07 01:24:50 +0800

    Update lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,13 +5,15 @@ return {
       icons_enabled = true,
       theme = 'onedark',
       -- section_separators = { left = '', right = '' },
-      component_separators = { left = '', right = '' },
-      -- component_separators = { left = '', right = ''},
+      -- component_separators = { left = '', right = '' },
+      -- component_separators = { left = '', right = '' },
+      -- component_separators = { left = '│', right = '│' },
+      component_separators = { left = '┊', right = '' },
       section_separators = { left = '', right = '' },
       symbols = {
         -- unix = '', -- e712
-        -- unix = '',  -- e70f
-        -- unix = '',  -- e711
+        -- dos = '',  -- e70f
+        -- mac = '',  -- e711
       }
     },
   },
