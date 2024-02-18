a21110c60c354665aa5f016ff049cc38955f5cd6
geloman <geloman-likes-rust@users.noreply.github.com>
2024-02-19 03:05:12 +0800

    Update trigger keys for vim-dadbod

--- nvim/lua/plugins/vim-dadbod.lua
+++ nvim/lua/plugins/vim-dadbod.lua
@@ -1,8 +1,6 @@
 return {
     "tpope/vim-dadbod",
     cmd = { "DB", "DBUI", "DBUIToggle" },
-    keys = function()
-        return { { "<leader>db", function() vim.cmd("DBUIToggle") end, desc = "DBUIToggle" } }
-    end,
+    keys = { { "<leader>db", function() vim.cmd("DBUIToggle") end, desc = "DBUIToggle" } },
     dependencies = { "kristijanhusak/vim-dadbod-ui", "kristijanhusak/vim-dadbod-completion" },
 }
